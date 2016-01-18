5f086f39b8b8707c8a14fcf65723d1fb8040e16c
Stein Fletcher <steinfletcher@gmail.com>
2016-01-18 01:28:56 +0000

    Add auth/security tests

--- src/test/scala/com/tapatron/feature/AuthFeatureTest.scala
+++ src/test/scala/com/tapatron/feature/AuthFeatureTest.scala
@@ -13,32 +13,32 @@ class AuthFeatureTest extends FeatureSpec {
 
   "Api security" should {
 
-//    "authenticate a user" in {
-//
-//      Given("An existing user")
-//      val user = User(username = "stein", password = "password")
-//      persistUsers(user)
-//
-//      When("The api receives a login request from user")
-//      val authHeader = SecurityUtils.authHeaderFromCredentials(Credentials(user.username, user.password))
-//      val response = server.httpPost(path = "/login",
-//        headers = Map("Authorization" -> authHeader),
-//        postBody = "",
-//        andExpect = Ok)
-//
-//      Then("A session cookie is returned")
-//      val authCookie = response.headerMap.get("Set-Cookie")
-//      authCookie.get should startWith regex "sid=+"
-//
-//      Then("The session cookie can be used to authenticate future requests")
-//      server.httpPost(path = "/post",
-//        headers = Map("Cookie" -> authCookie.get),
-//        postBody =
-//          """
-//             {"title": "some post"}
-//          """,
-//        andExpect = Created)
-//    }
+    "authenticate a user" in {
+
+      Given("An existing user")
+      val user = User(username = "stein", password = "password")
+      persistUsers(user)
+
+      When("The api receives a login request from user")
+      val authHeader = SecurityUtils.authHeaderFromCredentials(Credentials(user.username, user.password))
+      val response = server.httpPost(path = "/login",
+        headers = Map("Authorization" -> authHeader),
+        postBody = "",
+        andExpect = Ok)
+
+      Then("A session cookie is returned")
+      val authCookie = response.headerMap.get("Set-Cookie")
+      authCookie.get should startWith regex "sid=+"
+
+      Then("The session cookie can be used to authenticate future requests")
+      server.httpPost(path = "/post",
+        headers = Map("Cookie" -> authCookie.get),
+        postBody =
+          """
+             {"title": "some post"}
+          """,
+        andExpect = Created)
+    }
 
     "logout an already authenticated user" in {
 
@@ -61,7 +61,7 @@ class AuthFeatureTest extends FeatureSpec {
         andExpect = Ok)
 
       Then("The user is no longer authenticated")
-      Then("The session cookie can be used to authenticate future requests")
+      Then("Future requests will fail with 401")
       server.httpPost(path = "/post",
         headers = Map("Cookie" -> authCookie.get),
         postBody =
